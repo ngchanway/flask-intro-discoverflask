4691279c4554f2fe6afc1510e3dd94550e548d14
ngchanway <chanwayng@u.nus.edu>
2020-06-11 17:27:25 +0800

    add author_id

--- db_create.py
+++ db_create.py
@@ -5,10 +5,10 @@ from project.models import BlogPost
 db.create_all()
 
 # insert
-db.session.add(BlogPost('Good', "I'm good."))
-db.session.add(BlogPost('Well', "I'm well."))
-db.session.add(BlogPost('Flask', 'discoverflask.com'))
-db.session.add(BlogPost('postgres', 'we setup a local postgres instance'))
+db.session.add(BlogPost('Good', "I'm good.", 1))
+db.session.add(BlogPost('Well', "I'm well.", 1))
+db.session.add(BlogPost('Flask', 'discoverflask.com', 1))
+db.session.add(BlogPost('postgres', 'we setup a local postgres instance', 1))
 
 # commit the changes
 db.session.commit()
