48e4b0563d11a717a1a1ec14653b970970d5fe81
ngchanway <chanwayng@u.nus.edu>
2020-06-11 18:27:09 +0800

    create table users before table posts

--- db_create_posts.py
+++ db_create_posts.py
@@ -1,9 +1,6 @@
 from project import db
 from project.models import BlogPost
 
-# create the database and the db tables
-db.create_all()
-
 # insert
 db.session.add(BlogPost('Good', "I'm good.", 1))
 db.session.add(BlogPost('Well', "I'm well.", 1))
